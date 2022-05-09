334080ae2c4593ee892534155fc9bbe2c8fe1edd
Tejas M R <totejasmr@gmail.com>
2022-05-09 12:51:58 +0530

    Update App.tsx
    1. Replaced Tab2 with PhotoGallery
    2. Replaced icon of Tab2 with images
    3. Changed path of tab2 with photos

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { images, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
+import PhotoGallery from './pages/PhotoGallery';
 import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
@@ -44,8 +44,8 @@ const App: React.FC = () => (
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/photos">
+            <PhotoGallery />
           </Route>
           <Route path="/tab3">
             <Tab3 />
@@ -59,9 +59,9 @@ const App: React.FC = () => (
             <IonIcon icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="photos" href="/photos">
+            <IonIcon icon={images} />
+            <IonLabel>Photo Gallery</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
