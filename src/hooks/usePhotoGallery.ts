7e544bac3f60f9002f653ec1554573d80fa3ea7d
Tejas M R <totejasmr@gmail.com>
2022-05-09 12:49:16 +0530

    Create a usePhotoGallery hook to get camera

---
+++ src/hooks/usePhotoGallery.ts
@@ -0,0 +1,21 @@
+// import { useState, useEffect } from 'react';
+// import { isPlatform } from '@ionic/react';
+
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+// import { Filesystem, Directory } from '@capacitor/filesystem';
+// import { Storage } from '@capacitor/storage';
+// import { Capacitor } from '@capacitor/core';
+
+export function usePhotoGallery() {
+    const takePhoto = async () => {
+        const photo = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100,
+        });
+    };
+
+    return {
+        takePhoto,
+    };
+}
